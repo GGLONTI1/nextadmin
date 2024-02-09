a949360bf6c030051bb3c5fd2eec9dfed97c6ed7
GGLONTI1 <giorgiglonti23@gmail.com>
2024-02-09 18:20:36 +0400

    chartamde mivedit

--- app/ui/dashboard/sidebar/sidebar.jsx
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -13,6 +13,7 @@ import {
   MdLogout,
 } from "react-icons/md";
 import MenuLink from './menuLink/menuLink';
+import Image from 'next/image';
 
 const menuItems = [
   {
@@ -81,26 +82,29 @@ const menuItems = [
 const Sidebar = () => {
   return (
     <div className={styles.container}>
-      <ul>
-        {menuItems.map(cat => (
-
+      <div className={styles.user}>
+        <Image className={styles.userImage} src="/noavatar.png" alt="" width="50" height="50"/>
+        <div className={styles.userDetail}>
+          <span className={styles.username}>John Doe</span>
+          <span className={styles.userTitle}>Administrator</span>
+        </div>
+      </div>
+      <ul className={styles.list}>
+        {menuItems.map((cat) => (
           <li key={cat.title}>
-            <span className={styles.cat}>
-
-              {cat.title}
-
-            </span>
-            {cat.list.map(item => (
-              <MenuLink item={item}
-              />
+            <span className={styles.cat}>{cat.title}</span>
+            {cat.list.map((item) => (
+              <MenuLink item={item} key={item.title}/>
             ))}
           </li>
-
         ))}
       </ul>
+      <button className={styles.logout}>
+        <MdLogout />
+        Logout
+      </button>
     </div>
-
-  )
-}
+  );
+};
 
 export default Sidebar;
